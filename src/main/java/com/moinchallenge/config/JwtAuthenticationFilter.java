8fc6f9765d99341fe629cdde78c1e0a0fb799a8a
SangMoon <rs.moony@gmail.com>
2025-02-13 12:31:01 +0900

    refactor : 토큰 저장 시에 조건 추가

--- src/main/java/com/moinchallenge/config/JwtAuthenticationFilter.java
+++ src/main/java/com/moinchallenge/config/JwtAuthenticationFilter.java
@@ -8,11 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -33,21 +35,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
-        // 1) 요청 헤더에서 "Authorization" 추출
         String token = getTokenFromRequest(request);
 
-        // 2) 유효한 토큰이라면 -> 인증 정보 SecurityContext 에 세팅
         if (token != null && jwtService.validateToken(token)) {
             String userId = jwtService.getUserIdFromToken(token);
 
-            // 스프링 시큐리티가 사용하는 인증 객체
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(userId, null, List.of());
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userId, null, List.of());
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
-        // 3) 다음 필터 진행
         filterChain.doFilter(request, response);
     }
 }
